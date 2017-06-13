a1cf7303ceed9aa016cac6b14f6327d8836fce49
Brett Peskin <bpeskin3941@gmail.com>
2017-06-13 07:06:36 -0400

    Make comments realtime

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,13 +6,22 @@ class CommentsController < ApplicationController
 		@comment = @recipe.comments.build(comment_params)
 		@comment.chef = current_chef
 		if @comment.save
-			flash[:success] = "Comment was created successfully"
-			redirect_to recipe_path(@recipe)
+			ActionCable.server.broadcast "comments", render(partial: 'comments/comment', object: @comment)
+			# flash[:success] = "Comment was created successfully"
+			# redirect_to recipe_path(@recipe)
 		else
 			flash[:danger] = "There was a problem creating the comment"
 			redirect_to :back
 		end
 	end
+
+  def destroy
+    @recipe = Recipe.find_by(params[:recipe_id])
+    @comment = @recipe.comments.find_by(params[:id])
+    @comment.destroy
+    redirect_to recipe_path(@recipe)
+    flash[:success] = "Your comment was successfully deleted"
+  end
 
 	private
 
