def1b9ff4037fb3ae4c4251964836441d01c4778
Brett Peskin <bpeskin3941@gmail.com>
2017-06-14 11:17:28 -0400

    Complete ChefChat

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
 	delete '/logout', to: 'sessions#destroy'
 
 	resources :ingredients, except: [:destroy]
+	get '/chat', to: 'chatrooms#show'
+	resources :messages, only: [:create]
 
 	mount ActionCable.server => '/cable'
 end
